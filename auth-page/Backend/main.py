49d789a75aaaf1e9d0ff432703d3d1e512e26b56
Abenezer Molla <57775399+abenezer-molla@users.noreply.github.com>
2022-03-26 17:48:48 +0000

    User Table Migrated

--- auth-page/Backend/main.py
+++ auth-page/Backend/main.py
@@ -1,8 +1,11 @@
 from flask import Flask
-from flask_restx import Api, Resource
+
+from flask_restx import Api, Resource, fields
 from config import DevConfig
 from models import User
 from exts import db
+from flask_migrate import Migrate
+
 
 #from models import Recipe,User
 #from exts import db
@@ -13,19 +16,26 @@ from exts import db
 #from flask_cors import CORS
 
 
-app=Flask(__name__)
+
+'''
+app = Flask(__name__)
 
 app.config.from_object(DevConfig)
 
 db.init_app(app)
 
+migrate = Migrate(app, db)
+
 api=Api(app,doc='/docs')
+
 
 @api.route('/hello')
 
 class HelloResources(Resource):
     def get(self):
         return {"message":"Aben is Here"}
+
+
 
 
 @app.shell_context_processor
@@ -53,40 +63,35 @@ if __name__ == "__main__":
     app.run()
 
 
+
 '''
 
+
 def create_app(config):
-    app=Flask(__name__,static_url_path='/',static_folder='./client/build')
+
+
+    app=Flask(__name__)
     app.config.from_object(config)
 
-    CORS(app)
+    #CORS(app)
 
     db.init_app(app)
 
     migrate=Migrate(app,db)
-    JWTManager(app)
+    #JWTManager(app)
 
 
 
     api=Api(app,doc='/docs')
 
-    api.add_namespace(recipe_ns)
-    api.add_namespace(auth_ns)
-
-    @app.route('/')
-    def index():
-        return app.send_static_file('index.html')
-
-    @app.errorhandler(404)
-    def not_found(err):
-        return app.send_static_file('index.html')
+    #api.add_namespace(recipe_ns)
+    #api.add_namespace(auth_ns)
 
     #model (serializer)
     @app.shell_context_processor
     def make_shell_context():
         return {
             "db":db,
-            "Recipe":Recipe,
             "user":User
         }
 
@@ -94,4 +99,3 @@ def create_app(config):
 
     return app
 
-'''
