596d6a6881378b951af28ae69a3717c323c05125
Abenezer Molla <57775399+abenezer-molla@users.noreply.github.com>
2022-03-27 23:40:18 +0100

    Working Checkpoint

--- auth-page/src/Components/Login.js
+++ auth-page/src/Components/Login.js
@@ -1,9 +1,16 @@
-import React, {useRef} from 'react'
+import React, {useState} from 'react'
 import {Form, Button, Card} from 'react-bootstrap'
 
 function Login() {
-    const emailRef = useRef()
-    const passwordRef = useRef()
+    const [password, setPassword] = useState('')
+    const [email, setEmail] = useState('')
+
+    const handleSubmit = (e) => {
+        console.log("WORKS!")
+        console.log(email)
+        console.log(password)
+        e.preventDefault()
+    }
 
   return (
 <Card>
@@ -12,17 +19,29 @@ function Login() {
             <Form>
                 <Form.Group id = "email">
                     <Form.Label>Email</Form.Label>
-                    <Form.Control type = "email" ref  = {emailRef} required/>
+                    <Form.Control 
+                    type = "email" 
+                    placeholder='Email' 
+                    value = {email} 
+                    name = "email"  r
+                    onChange = {(e)=> {setEmail(e.target.value)}}
+                    equired/>
                 </Form.Group>
 
                 <Form.Group id = "password">
                     <Form.Label>Password</Form.Label>
-                    <Form.Control type = "password" ref  = {passwordRef} required/>
+                    <Form.Control 
+                    type = "password" 
+                    placeholder='Password' 
+                    value = {password} 
+                    name = "password" 
+                    onChange = {(e)=> {setPassword(e.target.value)}}
+                    required/>
                 </Form.Group>
                 <br/>
 
 
-                <Button className = "w-100" type  = "submit">Login</Button>
+                <Button onClick={handleSubmit} className = "w-100" type  = "submit">Login</Button>
             </Form>
         </Card.Body>
     </Card>
--- auth-page/src/Components/SignUp.js
+++ auth-page/src/Components/SignUp.js
@@ -1,13 +1,21 @@
-import React, {useRef} from 'react'
+import React, {useState} from 'react'
 import {Form, Button, Card} from 'react-bootstrap'
 
 import {Link} from 'react-router-dom'
 
 function SignUp() {
 
-const emailRef = useRef()
-const passwordRef = useRef()
-const passwordconfirmRef = useRef()
+const [email, setEmail] = useState('')
+const [password, setPassword] = useState('')
+const [confirmpassword, setConfirmPassword] = useState('')
+
+const handleSubmit = (e) => {
+    console.log("WORKS!")
+    console.log(email)
+    console.log(password)
+    e.preventDefault()
+}
+
   return (
 
     <>
@@ -18,20 +26,41 @@ const passwordconfirmRef = useRef()
             <Form>
                 <Form.Group id = "email">
                     <Form.Label>Email</Form.Label>
-                    <Form.Control type = "email" ref  = {emailRef} required/>
+                    <Form.Control 
+                    type = "email" 
+                    placeholder='Email'  
+                    value = {email} 
+                    name = "email" 
+                    onChange = {(e)=> {setEmail(e.target.value)}}
+                    
+                    />
                 </Form.Group>
 
                 <Form.Group id = "password">
                     <Form.Label>Password</Form.Label>
-                    <Form.Control type = "password" ref  = {passwordRef} required/>
+                    <Form.Control 
+                    type = "password" 
+                    placeholder='Password'  
+                    value = {password}  
+                    name = "password"
+                    onChange = {(e)=> {setPassword(e.target.value)}}
+                    
+                    />
                 </Form.Group>
 
                 <Form.Group id = "passwordconfirm">
                     <Form.Label>Password Confirmation</Form.Label>
-                    <Form.Control type = "password" ref  = {passwordconfirmRef} required/>
+                    <Form.Control 
+                    type = "password" 
+                    placeholder=' Confirm Password'  
+                    value = {confirmpassword} 
+                    name = "confirmpassword" 
+                    onChange = {(e)=> {setConfirmPassword(e.target.value)}}
+                    
+                    />
                 </Form.Group>
                 <br/>
-                <Button className = "w-100" type  = "submit">Sign Up</Button>
+                <Button onClick = {handleSubmit} className = "w-100" type  = "submit" >Sign Up</Button>
             </Form>
         </Card.Body>
     </Card>
